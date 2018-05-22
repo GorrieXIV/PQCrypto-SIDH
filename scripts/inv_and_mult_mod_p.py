6cb1ce11d41d800aecf92db2c0786b90971ab6a2
GorrieXIV <rob-gxiv@hotmail.com>
2018-05-21 20:10:36 -0400

    updated tests and python script -gxiv

--- scripts/inv_and_mult_mod_p.py
+++ scripts/inv_and_mult_mod_p.py
@@ -4,11 +4,11 @@ comp = []
 bit = []
 
 # p stores the working modulus
-p = 'C968549F878A8EEB59B1A13F7CC76E3EE9867D6EBE876DA92B5045CB257480842909F97BADC6685606FE5D541F71C0E1'
+p = '06FE5D541F71C0E12909F97BADC668562B5045CB25748084E9867D6EBE876DA959B1A13F7CC76E3EC968549F878A8EEB'
 p = int(p, 16)
 
 i = 0
-with open("abcomp_output") as file:
+with open("scripts/abcomp_output") as file:
 	for line in file:
 		if (i == 0):
 			line = line.strip("a: ")
@@ -25,11 +25,20 @@ with open("abcomp_output") as file:
 		i = (i + 1) % 4
 
 for q in range(0, len(a)):
-    if (bit[q] == 1):
-        ainv = pow (a[q], p-2, p)
-        rcomp = (ainv * b[q]) % p
-        print(str(comp[q] == rcomp))
-    else:
-        binv = pow(b[q], p-2, p)
-        rcomp = (binv * a[q]) % p
-        print(str(comp[q] == rcomp))
+    #if (bit[q] == 1):
+    #ainv = pow (a[q], p-2, p)
+    #binv = pow (b[q], p-2, p)
+    #acomp = (ainv * b[q]) % p
+    bcomp = (b[q] * a[q]) % p
+    print('a*b: {:x}'.format(bcomp))
+    #print('binv*a: {:x}'.format(bcomp))
+    print('original: {:x}'.format(comp[q] % p))
+    print()
+		#print(str(comp[q] == rcomp))
+    #else:
+    #    binv = pow(b[q], p-2, p)
+    #    rcomp = (binv * a[q]) % p
+    #    print('{:x}'.format(rcomp))
+    #    print('{:x}'.format(comp[q]))
+    #    print()
+        #print(str(comp[q] == rcomp))
