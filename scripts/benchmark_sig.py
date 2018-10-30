dfefc59ee04181f8ba960a74b6ded178b11bf7a5
GorrieXIV <rob@arkelltech.com>
2018-10-29 21:34:51 -0400

    added compression+batching tracking to benchmarking script

--- scripts/benchmark_sig.py
+++ scripts/benchmark_sig.py
@@ -19,11 +19,12 @@ def plot_and_save(sign_points, verify_points, title, file_name):
 
 
 sig_runs = 2
-batched_runs = 3
-compressed_runs = 1
+batched_runs = 2
+compressed_runs = 2
+CB_runs = 2
 
 # run signature test
-exec_call = ".././sig_test " + str(sig_runs) + " " + str(batched_runs) + " " + str(compressed_runs) + " > signature_benchmarks"
+exec_call = ".././sig_test " + str(sig_runs) + " " + str(batched_runs) + " " + str(compressed_runs) + " " + str(CB_runs) + " > signature_benchmarks"
 print("running \"" + exec_call + "\":")
 print("...please wait... \n")
 os.system(exec_call)
@@ -43,6 +44,11 @@ compressed_sign_avg = 0
 compressed_verify_avg = 0
 compressed_sign_stddev_list = []
 compressed_verify_stddev_list = []
+
+CB_sign_avg = 0
+CB_verify_avg = 0
+CB_sign_stddev_list = []
+CB_verify_stddev_list = []
 
 i = 0
 j = 0 
@@ -74,15 +80,24 @@ with open("signature_benchmarks") as file:
         batched_verify_avg += int(line)
         batched_verify_stddev_list.append(int(line))
 
-    else:
+    elif (i < sig_runs + batched_runs + compressed_runs):
       if (j == 0):
         print("cycle time for compressed signature " + str(i+1-sig_runs-batched_runs) + " sign: " + str(line)[:-1])
         compressed_sign_avg += int(line)
         compressed_sign_stddev_list.append(int(line))
       elif(j == 1):
-        print("cycle time for compressed signature " + str(i+1-sig_runs-batched_runs) + " sign: " + str(line)[:-1])
+        print("cycle time for compressed signature " + str(i+1-sig_runs-batched_runs) + " verify: " + str(line)[:-1])
         compressed_verify_avg += int(line)
         compressed_verify_stddev_list.append(int(line))
+    else:
+      if (j == 0):
+        print("cycle time for CB signature " + str(i+1-sig_runs-batched_runs-compressed_runs) + " sign: " + str(line)[:-1])
+        CB_sign_avg += int(line)
+        CB_sign_stddev_list.append(int(line))
+      elif(j == 1):
+        print("cycle time for CB signature " + str(i+1-sig_runs-batched_runs-compressed_runs) + " verify: " + str(line)[:-1])
+        CB_verify_avg += int(line)
+        CB_verify_stddev_list.append(int(line))
 
     j += 1
 print("-------------------------------------------------------------")
@@ -96,13 +111,16 @@ batched_verify_stddev = 0
 compressed_sign_stddev = 0
 compressed_verify_stddev = 0
 
+CB_sign_stddev = 0
+CB_verify_stddev = 0
+
 # compute and display vanilla signature average runtime and standard deviation
 if (sig_runs != 0):
   plot_and_save(vanilla_sign_stddev_list, vanilla_verify_stddev_list, 'Performance Measurements - Unmodified Scheme', 'vanilla_cycles.pdf')
   vanilla_sign_avg = vanilla_sign_avg / sig_runs
   vanilla_verify_avg = vanilla_verify_avg / sig_runs
-  print("signature sign average: " + str(vanilla_sign_avg))
-  print("signature verify average: " + str(vanilla_verify_avg))
+  print("vanilla signature sign average: " + str(vanilla_sign_avg))
+  print("vanilla signature verify average: " + str(vanilla_verify_avg))
   for q in range (0, sig_runs-1):
     vanilla_sign_stddev_list[q] = (vanilla_sign_stddev_list[q] - vanilla_sign_avg)**(2)
     vanilla_verify_stddev_list[q] = (vanilla_verify_stddev_list[q] - vanilla_verify_avg)**(2)
@@ -148,4 +166,20 @@ if (compressed_runs != 0):
   print("compressed signature verify standard deviation: " + str(compressed_verify_stddev))
 
 
+# compute and display C+B signature average runtime and standard deviation
+if (CB_runs != 0): 
+  plot_and_save(CB_sign_stddev_list, CB_verify_stddev_list, 'Performance Measurements - Compressed Signatures with Batching', 'CB-cycles.pdf')
+  CB_sign_avg = CB_sign_avg / CB_runs
+  CB_verify_avg = CB_verify_avg / CB_runs
+  print("C + B signature sign average: " + str(CB_sign_avg))
+  print("C + B signature verify average: " + str(CB_verify_avg))
+  for q in range (0, CB_runs-1):
+    CB_sign_stddev_list[q] = (CB_sign_stddev_list[q] - CB_sign_avg)**(2)
+    CB_verify_stddev_list[q] = (CB_verify_stddev_list[q] - CB_verify_avg)**(2)
+    CB_sign_stddev += CB_sign_stddev_list[q]
+    CB_verify_stddev += CB_verify_stddev_list[q]
+  CB_sign_stddev = math.sqrt(CB_sign_stddev / CB_runs)
+  CB_verify_stddev = math.sqrt(CB_verify_stddev / CB_runs)
+  print("C + B signature sign standard deviation: " + str(CB_sign_stddev))
+  print("C + B signature verify standard deviation: " + str(CB_verify_stddev))
 
